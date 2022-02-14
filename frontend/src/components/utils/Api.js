0ac675024e1304324fa70d989635946701c7a808
Sergay <tsvetkoffgolod@yandex.ru>
2022-02-14 19:08:02 +0300

    Api set

--- frontend/src/components/utils/Api.js
+++ frontend/src/components/utils/Api.js
@@ -92,7 +92,7 @@ class Api {
 const apiClass = new Api({                             // записываем стартовый экземпляп
   baseUrl: 'https://api.mestoproject.students.nomoredomains.xyz',
   headers: {
-    'authorization': `${localStorage.getItem('token')}`,
+    'Authorization': `Bearer ${localStorage.getItem('token')}`,
     'Content-Type': 'application/json'
   }
 });
