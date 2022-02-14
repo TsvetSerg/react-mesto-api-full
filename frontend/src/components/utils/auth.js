ca2e96932af6e25440660f50f31b374b84f7d784
Sergay <tsvetkoffgolod@yandex.ru>
2022-02-14 17:21:03 +0300

    token 2

--- frontend/src/components/utils/auth.js
+++ frontend/src/components/utils/auth.js
@@ -52,7 +52,7 @@ export const getToken = (token) => {
     method: 'GET',
     headers: {
       "Content-Type": "application/json",
-      "authorization" : `${token}`
+      "Authorization" : `Bearer ${token}`
     }
   })
   .then((res) => {
