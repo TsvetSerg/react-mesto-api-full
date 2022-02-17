95b84218a09c06e5eca065f364b77ff653e75e3e
Sergay <tsvetkoffgolod@yandex.ru>
2022-02-17 13:23:25 +0300

    setting likke 9

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -78,7 +78,7 @@ function App() {
 
 
   function handleCardLike(card) {                 // Функуция поосстановки лайков
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
 
     if (!isLiked) {
       apiClass.putLikeCard(card._id)
